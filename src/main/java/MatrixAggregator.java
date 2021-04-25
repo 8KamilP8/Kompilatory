9e60a4abb2226e2d94243a13ceebe94046f512ec
Anton <43788645+bohatiuk@users.noreply.github.com>
2021-04-25 22:32:20 +0200

    add MatrixAggregator

---
+++ src/main/java/MatrixAggregator.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+
+class MatrixElement {
+    public int v = 0; // wartosc pikesla (kolor)
+
+    MatrixElement(int v) {
+        this.v = v;
+    }
+}
+
+// lista wierszy
+public class MatrixAggregator {
+
+
+    public ArrayList<ArrayList<MatrixElement>> matrix;
+    public int size = 10;
+
+    MatrixAggregator() {
+        matrix = new ArrayList<ArrayList<MatrixElement>>();
+        for (int i = 0; i < size; i++) {
+            matrix.add(new ArrayList<MatrixElement>());
+            for (int j = 0; j < size; j++) {
+                matrix.get(i).add(new MatrixElement(0));
+            }
+        }
+    }
+
+    MatrixElement getValue(int x, int y) {
+        return matrix.get(y).get(x);
+    }
+
+    void setValue(int x, int y, MatrixElement value) {
+        matrix.get(y).set(x, value);
+    }
+
+    void flush() {
+        matrix = null;
+
+        matrix = new ArrayList<ArrayList<MatrixElement>>();
+        for (int i = 0; i < size; i++) {
+            matrix.add(new ArrayList<MatrixElement>());
+            for (int j = 0; j < size; j++) {
+                matrix.get(i).add(new MatrixElement(0));
+            }
+        }
+    }
+
+    void plot(int x, int y, int color) {
+        setValue(x, y, new MatrixElement(color));
+    }
+
+    void print() {
+        for (var r : matrix) {
+            for (var e : r) {
+                System.out.print(e.v + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
